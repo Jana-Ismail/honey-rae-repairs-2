ef8731bcc99fdf9c6df01ed6b88361259024922a
Jana Ismail <janaismail@janas-air.lan>
2024-07-19 15:52:29 -0500

    Implemented Count button that renders count as it changes | Rendered JSX Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
-import "./index.css"
-
 export const App = () => {
-  return(<h1>Welcome to Your First React App!</h1>)
+
+  return (
+    <>
+      <h1>Hello!</h1>
+      <div>React is amazing!</div>
+      <button className="btn-secondary" onClick={handleBtnClick}>
+        Click me!
+      </button>
+      <div>
+        Count: {count}
+      </div>
+    </> 
+  )
 }
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+{/* const [count, setCount] = useState(0)
+
+const handleBtnClick = () => {
+  setCount(count + 1)
+} */}
+
+// import { useState } from "react"
