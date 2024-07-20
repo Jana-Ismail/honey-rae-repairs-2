673455c14bb1de325a31d1eaa794ce0748a65a14
Jana Ismail <janaismail@janas-air.lan>
2024-07-19 23:20:31 -0500

    Implemented Emergency and Show All ticket views in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,67 @@
+import { useEffect, useState } from "react"
+import { getAllTickets } from "./services/ticketService"
+import './App.css'
+
 export const App = () => {
+const [allTickets, setAllTickets] = useState([])
+const [showEmergencyOnly, setShowEmergencyOnly] = useState(false)
+const [filteredTickets, setFilteredTickets] = useState([])
+
+useEffect(() => {
+  getAllTickets().then((ticketsArr) => {
+    setAllTickets(ticketsArr)
+  })
+}, [])
+
+useEffect(() => {
+  getAllTickets().then((ticketsArr) => {
+    if (showEmergencyOnly) {
+      const emergencyTickets = ticketsArr.filter((ticket) => ticket.emergency)
+      setFilteredTickets(emergencyTickets)
+    } else {
+      setFilteredTickets(allTickets)
+    }
+
+  })
+}, [showEmergencyOnly, allTickets])
 
   return (
-    <>
-      <h1>Hello!</h1>
-      <div>React is amazing!</div>
-      <button className="btn-secondary" onClick={handleBtnClick}>
-        Click me!
+    <div className="tickets-container">
+      <h2>Tickets</h2>
+      <button 
+        className="filter-btn btn-primary"
+        onClick={() => setShowEmergencyOnly(true)}
+      >
+        Emergency
       </button>
-      <div>
-        Count: {count}
-      </div>
-    </> 
+      <button 
+        className="filter-btn btn-secondary"
+        onClick={() => setShowEmergencyOnly(false)}
+      >
+        Show All
+      </button>
+      <article className="tickets">
+        {filteredTickets.map((ticket) => {
+            return (
+              <section className="ticket" key={ticket.id}>
+                <header className="ticket-info">#{ticket.id}</header>
+                <div>{ticket.description}</div>
+                <footer>
+                  <div>
+                    <div className="ticket-info">emergency </div>
+                    <div>{ticket.emergency ? "yes" : "no"}</div>
+                  </div>
+                </footer>
+              </section>
+            )
+          })
+        }
+      </article>
+    </div> 
   )
+
 }
+
 
       
 
@@ -32,10 +81,4 @@ export const App = () => {
 
 
 
-{/* const [count, setCount] = useState(0)
 
-const handleBtnClick = () => {
-  setCount(count + 1)
-} */}
-
-// import { useState } from "react"
