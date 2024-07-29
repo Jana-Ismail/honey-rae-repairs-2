78c3704b818e357f4b2e3c67d93232198687873b
Jana Ismail <janaismail@janas-air.lan>
2024-07-28 19:22:43 -0500

    Added EmployeeForm component to allow an employee to edit profile info

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -5,3 +5,15 @@ export const getEmployees = () => {
 export const getEmployeeByUserId = (userId) => {
     return fetch(`http://localhost:8088/employees?userId=${userId}&_expand=user&_embed=employeeTickets`).then(res => res.json())
 }
+
+export const updateEmployee = (employeeObj) => {
+    return fetch(
+        `http://localhost:8088/employees/${employeeObj.id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(employeeObj)
+        }
+    )
+}
